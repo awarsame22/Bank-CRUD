dee9cd5992babe0b68e4b2eaf1c08e2b7a159109
Aisha Warsame <aishawarsame@Aishas-MacBook-Air.local>
2022-11-06 21:35:30 -0600

    implemented if for c

--- src/Bank.java
+++ src/Bank.java
@@ -16,3 +16,17 @@ public class Bank {
             System.out.println("Enter 'S' to save your transactions");
             System.out.println("Enter 'Q' to quit");
             choice = scan.nextLine();
+
+            if (choice.equalsIgnoreCase("C")) {
+                System.out.println("Enter the id of your transaction: ");
+                int id = Integer.parseInt(scan.nextLine());
+                System.out.println("Enter the name of the account holder: ");
+                String name = scan.nextLine();
+                System.out.println("Enter the description of your transaction: ");
+                String description = scan.nextLine();
+                System.out.println("Enter the price of your transaction: ");
+                double price = Double.parseDouble(scan.nextLine());
+
+                transaction = new Transaction(id, name, description, price);
+                transactions.put(id, transaction);
+                System.out.println(transactions);
