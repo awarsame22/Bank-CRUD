af9bc06da68adb0a72ba70d732bee1721b226407
Aisha Warsame <aishawarsame@Aishas-MacBook-Air.local>
2022-11-06 21:34:05 -0600

    implemented do-while

--- src/Bank.java
+++ src/Bank.java
@@ -5,3 +5,14 @@ public class Bank {
     String choice;
 
     Transaction transaction;
+    public void createMethod() {
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            System.out.println("Enter 'C' to create a transaction");
+            System.out.println("Enter 'R' to read your transactions");
+            System.out.println("Enter 'U' to update a transaction");
+            System.out.println("Enter 'D' to delete a transaction");
+            System.out.println("Enter 'S' to save your transactions");
+            System.out.println("Enter 'Q' to quit");
+            choice = scan.nextLine();
