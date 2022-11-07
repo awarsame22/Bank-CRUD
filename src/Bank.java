58870bafc6746f0d0bcffef59b6e0c44cc4487e5
Aisha Warsame <aishawarsame@Aishas-MacBook-Air.local>
2022-11-06 21:33:12 -0600

    started manager class

---
+++ src/Bank.java
@@ -0,0 +1,7 @@
+import java.util.HashMap;
+import java.util.Scanner;
+public class Bank {
+    HashMap<Integer, Transaction> transactions = new HashMap<>();
+    String choice;
+
+    Transaction transaction;
